cafe2fdb66370a2b15b2b6fb878301f30850dc03
Shnyamsky <ivanov.ilya-iv@yandex.ru>
2022-05-17 02:07:11 +0500

    fix conflicts, drop rules from .eslintrc.js

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,9 +7,6 @@ module.exports = {
         "files": ["**/*.js?(x)"],
         "rules": {
   // ******** add ignore rules here *********
-          "react/no-unescaped-entities": "off",
-          "react/display-name": "off",
-          "react/prop-types": "off",
           "react-hooks/exhaustive-deps": "off"
         }
       }
